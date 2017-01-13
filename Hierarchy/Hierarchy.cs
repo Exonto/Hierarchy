ae0e7e4d692ffde71b44044da2057c3f5b466f52
Tyler <tylersyme@gmail.com>
2017-01-13 11:09:26 -0800

    Improved Functionality

--- Hierarchy/Hierarchy.cs
+++ Hierarchy/Hierarchy.cs
@@ -11,7 +11,7 @@ namespace HierarchyCollection
 		/// <summary>
 		/// This represents the first node in the <c>Hierarchy</c>
 		/// </summary>
-		public Node<E> _rootNode;
+		private Node<E> _rootNode;
 
 		public Hierarchy() { }
 
@@ -42,6 +42,11 @@ namespace HierarchyCollection
 			}
 		}
 
+		/// <summary>
+		/// Returns the child nodes of the given element.
+		/// </summary>
+		/// <returns>The child nodes.</returns>
+		/// <param name="parent">Parent.</param>
 		public List<Node<E>> GetChildNodes(E parent)
 		{
 			Node<E> parentNode = this.GetNode(parent, this._rootNode);
@@ -69,6 +74,10 @@ namespace HierarchyCollection
 
 		/// <summary>
 		/// Returns a node which is located based upon matching elements.
+		/// 
+		/// The start node indicates the node whose children will be
+		/// searched through for a matching pair. To check the entire
+		/// <c>Hierarchy</c>, use the root node.
 		/// </summary>
 		/// <returns>The node.</returns>
 		/// <param name="element">Element.</param>
@@ -97,13 +106,86 @@ namespace HierarchyCollection
 		}
 
 		/// <summary>
+		/// This allows the Hierarchy to have a "nested" or "sub" Hierarchy
+		/// extracted from it.
+		/// This is allowed because all Hierarchies are composed of nothing
+		/// more than other smaller Hierarchies.
+		/// 
+		/// The sub Hierarchy that is returned is a copy and changes made to 
+		/// this copy will not affect the original.
+		/// </summary>
+		/// <returns>The nested.</returns>
+		/// <param name="element">Element.</param>
+		public Hierarchy<E> GetNested(E element)
+		{
+			Node<E> startNode = this.GetNode(element);
+			Hierarchy<E> nested = new Hierarchy<E>(element);
+			return this.GetNested(nested, nested._rootNode,  startNode);
+		}
+
+		/// <summary>
+		/// This is an internal helper method which handles most of the actual
+		/// logic for Hierarchy.GetNested(E element).
+		/// </summary>
+		/// <returns>The nested.</returns>
+		/// <param name="nested">Nested.</param>
+		/// <param name="nestedSentry">Nested sentry.</param>
+		/// <param name="originalSentry">Original sentry.</param>
+		private Hierarchy<E> GetNested(Hierarchy<E> nested, 
+		                               Node<E> nestedSentry, 
+		                               Node<E> originalSentry)
+		{
+			// This method takes two nodes: Nested Sentry and Original Sentry.
+			// The Nested Sentry represents the node currently being observed in
+			// the Nested Hierarchy which is being built.
+			// The Original Sentry represents the node currently being observed in
+			// the Original Hierachy from which the Nested Hierarchy is being built
+			// from.
+			//
+			// These two nodes allow for quick copying and, more importantly, traversal
+			// of both Hierarchies. The Nested Sentry node's job is to "follow" the
+			// Original Sentry around and copy over that sentry's element.
+
+			// Confirms that the original sentry it not null
+			if (originalSentry == null) { return null; }
+
+			// Goes through each of the original sentry's child nodes
+			foreach (Node<E> child in originalSentry.ChildNodes)
+			{
+				// Adds the current child's element to the nested hierarchy
+				// via the nested sentry
+				Node<E> newNestedSentry = nestedSentry.AddChild(child.Element);
+
+				this.GetNested(nested, newNestedSentry, child);
+			}
+
+			return nested;
+		}
+
+// -----------------------------------------------------------------------------
+// Hierarchy Properties
+// -----------------------------------------------------------------------------
+
+		/// <summary>
 		/// Returns the root element of this <c>Hierarchy</c>.
 		/// </summary>
 		/// <value>The root.</value>
 		public E Root
 		{
 			get { return this._rootNode.Element; }
+			set { this._rootNode.Element = value; }
 		}
+
+		/// <summary>
+		/// Gets the root node of this <c>Hierarchy</c>.
+		/// 
+		/// </summary>
+		/// <value>The root node.</value>
+		public Node<E> RootNode
+		{
+			get { return this._rootNode; }
+		}
+
 // -----------------------------------------------------------------------------
 
 		/// <summary>
@@ -133,12 +215,11 @@ namespace HierarchyCollection
 
 			private E _element;
 
-			// Only accessible by the outer class
 			private List<Node<E>> _children = new List<Node<E>>();
 
 			// Keeps a cached record of the elements contained within this node's
 			// child nodes. This is done so that acquiring these elements from
-			// their respective nodes is an quick process
+			// their respective nodes is a quick process
 			private List<E> cachedChildElements = new List<E>();
 
 			public Node(E element)
@@ -169,7 +250,23 @@ namespace HierarchyCollection
 				return childNode;
 			}
 
+			/// <summary>
+			/// Can add a variable number of children to this node.
+			/// </summary>
+			/// <param name="elements">Elements.</param>
 			public void AddChildren(params E[] elements)
+			{
+				foreach (E element in elements)
+				{
+					this.AddChild(element);
+				}
+			}
+
+			/// <summary>
+			/// Will add all of the given elements to this node as children.
+			/// </summary>
+			/// <param name="elements">Elements.</param>
+			public void AddChildren(List<E> elements)
 			{
 				foreach (E element in elements)
 				{
@@ -180,15 +277,15 @@ namespace HierarchyCollection
 			/// <summary>
 			/// Returns whether this node has no children.
 			/// </summary>
-			/// <returns><c>false</c>, if node has children, <c>true</c> otherwise.</returns>
+			/// <returns><c>True</c>, if node has no children, <c>false</c> otherwise.</returns>
 			public bool IsLeaf()
 			{
 				return this._children.Count == 0;
 			}
 
 			/// <summary>
-			/// Returns whether or not this node contains a child node which contains
-			/// the given element.
+			/// Returns whether or not this node contains a child node which 
+			/// contains the given element.
 			/// </summary>
 			/// <returns><c>true</c>, if a child has this element, <c>false</c> otherwise.</returns>
 			/// <param name="element">Element.</param>
@@ -238,25 +335,31 @@ namespace HierarchyCollection
 				set { this._element = value; }
 			}
 
+			/// <summary>
+			/// Returns a list of this node's children's elements.
+			/// </summary>
+			/// <value>The children.</value>
 			public List<E> Children
 			{
-				get 
-				{
-					// Returns the elements contained within each child node by
-					// accessing the cache of these elements rather than looping
-					// through each child node
-					return cachedChildElements;
-				}
+				// Returns the elements contained within each child node by
+				// accessing the cache of these elements rather than looping
+				// through each child node
+				get { return cachedChildElements; }
 			}
 
+			/// <summary>
+			/// Returns a list of this node's child nodes.
+			/// </summary>
+			/// <value>The child nodes.</value>
 			public List<Node<E>> ChildNodes
 			{
-				get 
-				{
-					return this._children;
-				}
+				get { return this._children; }
 			}
 
+			/// <summary>
+			/// Returns this node's parent node.
+			/// </summary>
+			/// <value>The parent.</value>
 			public Node<E> Parent
 			{
 				get { return this._parent; }
